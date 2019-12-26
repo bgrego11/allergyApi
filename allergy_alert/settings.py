f7e0c167141afa3a93303aa735d6c4f4fcf55271
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2019-12-26 18:22:47 -0500

    drf permissions

--- allergy_alert/settings.py
+++ allergy_alert/settings.py
@@ -85,9 +85,12 @@ WSGI_APPLICATION = 'allergy_alert.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_PERMISSION_CLASSES': [
-   'rest_framework.permissions.AllowAny',
-]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
 }
 
 
