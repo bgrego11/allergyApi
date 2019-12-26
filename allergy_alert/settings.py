3a78bcd59f0c4476033106d7ba6055d6cd003d91
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2019-12-26 17:21:06 -0500

    static fix

--- allergy_alert/settings.py
+++ allergy_alert/settings.py
@@ -135,10 +135,14 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
