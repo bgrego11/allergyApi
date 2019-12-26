cb0a4642c939ef6ada14f696c1854d6b7fd01938
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2019-12-26 17:15:37 -0500

    whitenoise fix

--- allergy_alert/settings.py
+++ allergy_alert/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorag'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
